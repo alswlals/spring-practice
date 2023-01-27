e144e88c3e090687e910a2d0d0139d39578c5ed5
awlals <awlals616@gmail.com>
2023-01-27 16:04:47 +0900

    helloSpring #5/중간에 줌 꺼진 부분 확인 다시 하기,,,~

--- hellospring/src/main/java/com/douzone/hellospring/controller/UserController.java
+++ hellospring/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -3,6 +3,7 @@ package com.douzone.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -13,16 +14,49 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+//	
+//	@ResponseBody
+//	@RequestMapping(value="/join", method=RequestMethod.GET) /*value 미입력시 디폴트값인디 입력하면 URL임 */
+//	public String join() {
+//		return "UserController.join()";
+//	}
 	
-	@ResponseBody
+
 	@RequestMapping(value="/join", method=RequestMethod.GET) /*value 미입력시 디폴트값인디 입력하면 URL임 */
 	public String join() {
-		return "UserController.join()";
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	/* setter 필수? 들고오고자 하는 거랑일므 같아야하고 ,, , */
+	@RequestMapping(value="/join", method=RequestMethod.POST) /* POST: DB insert */
+	public String join(UserVo userVo) {
+		System.out.println(userVo);
+		return "redirect:/";
 	}
 	
 	@ResponseBody
+	@RequestMapping("/update")
+	public String update(@RequestParam(value="n") String name) { /*String일 경우 defaultValue에 빈값을 넣어 null값 안 만들기 */
+		/**
+		 * 
+		 * 만일 n이라는 이름의 URL 파라미터가 없으면
+		 * 400 Bad request error가 발생
+		 * 
+		 * */
+		return "UserController.update("+ name+")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n", required=true, defaultValue="") String name) { /*String일 경우 defaultValue에 빈값을 넣어 null값 안 만들기 */
+
+		return "UserController.update("+ name+")";
+	}
+	
+	
+	@ResponseBody
 	@RequestMapping("/list")
-	public String list() {
-		return "UserController.list()";
+	public String list(@RequestParam(value="p",required=true, defaultValue="1") int pageNo) { /*String일 경우 defaultValue에 빈값을 넣어 null값 안 만들기 */
+		return "UserController.list("+ pageNo+")";
 	}
 }

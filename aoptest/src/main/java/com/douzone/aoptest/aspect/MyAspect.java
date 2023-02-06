0824b0892fe40e2a5088f988c7fa72b2b114e555
awlals <awlals616@gmail.com>
2023-02-06 11:30:17 +0900

    auth

--- aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
@@ -13,41 +13,40 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyAspect {
 	
-	@Before("execution(public com.douzone.aoptest.vo.ProductVo com.douzone.aoptest.service.ProductService.find(String)")
+	@Before("execution(public com.douzone.aoptest.vo.ProductVo com.douzone.aoptest.service.ProductService.find(String))")
 	public void adviceBefore() {
-		System.out.println("---------- Before Advice -------------");
+		System.out.println("--- Before Advice ---");
 	}
 	
-	@After("execution(* *..*.ProductService.find(..)")
+	@After("execution(* *..*.ProductService.find(..))")
 	public void adviceAfter() {
-		System.out.println("---------- After Advice -------------");
+		System.out.println("--- After Advice ---");
 	}
-	
-	@AfterReturning("execution(* *..*.ProductService.*(..)")
+
+	@AfterReturning("execution(* *..*.ProductService.*(..))")
 	public void adviceAfterReturning() {
-		System.out.println("---------- After Returning Advice -------------");
+		System.out.println("--- AfterReturning Advice ---");
 	}
-	
-	@AfterThrowing(value = "execution(* *..*.*.*(..)", throwing="ex")
+
+	@AfterThrowing(value="execution(* *..*.*.*(..))", throwing="ex")
 	public void adviceAfterThrowing(Throwable ex) {
-		System.out.println("---------- After Throwing Advice "+ ex +" -------------");
+		System.out.println("--- AfterThrowing Advice " + ex + " ---");
 	}
 	
-	@Around("execution(* *..*.ProductService.*(..)")
+	@Around("execution(* *..*.ProductService.*(..))")
 	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
 		/* Before */
-		System.out.println("---------- Around(Before) Advice-------------");
+		System.out.println("--- Around(Before) Advice ---");
 		
 		/* Point Cut Method 실행 */
-		// 있는 파라미터 그대로 실행
-		 Object result = pjp.proceed();
 		
-		// 파라미터 바꾸고 싶을 때 (조작해야 할 때 )
-//		 Object[] params = {"Camera"};
-//		 Object result = pjp.proceed(params);
+		// Object[] params = {"Camera"}; 
+		// Object result = pjp.proceed(params);
+		
+		Object result = pjp.proceed();
 		
 		/* After */
-		System.out.println("---------- Around(After) Advice-------------");
+		System.out.println("--- Around(After) Advice ---");
 		
 		return result;
 	}

92f33d4ded5f533b6ac6f3ff412f783b63e6cc0f
awlals <awlals616@gmail.com>
2023-02-10 13:03:44 +0900

    container #4

--- container/src/main/java/com/douzone/container/user/config/AppConfig.java
+++ container/src/main/java/com/douzone/container/user/config/AppConfig.java
@@ -1,17 +1,18 @@
 package com.douzone.container.user.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.douzone.container.user.User;
+import com.douzone.container.user.Friend;
 
 @Configuration
-//@ComponentScan(basePackages= {"com.douzone.container.user"})
+@ComponentScan(basePackages= {"com.douzone.container.user"})
 public class AppConfig {
 	
 	@Bean
-	public User user() {
-		return new User();
+	public Friend friend() {
+		return new Friend("Loopy");
 	}
 
 }
